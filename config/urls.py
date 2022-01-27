7b04e682e32680d76c79ffbe5aef5d2c79ac19a5
NohGaSeong <noyohanx@naver.com>
2022-01-27 19:46:07 +0900

    SmallChapter04

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from pybo import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # pybo/ URL이 요청되면 views.index 를 호출하라는 매핑을 추가.
-    # views.index 는 views.py 파일의 index 함수를 의미한다.
-    path('pybo/', views.index),
-    # pybo 라고 입력했을때 pybo/ 로 변환될텐데 이것은 URL을 정규화하는 장고의 기능이다.
+    path('pybo/', include('pybo.urls')), 
 ]
